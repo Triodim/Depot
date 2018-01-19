f410dcc01129bac9df3d6e87ca0d72ee7ba6be88
Dimm <tds050680@gmail.com>
2018-01-19 15:42:10 +0200

    Store

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
-  validates :title, length: { minimum: 80 , message: 'must be more then 80 symbols'}
+  validates :title, length: { minimum: 8 , message: 'must be more then 8 symbols'}
   validates :image_url, allow_blank: true, format: {
       with: %r{\.(gif|jpg|png)\Z}i,
       message: 'must be a URL for GIF, JPG or PNG image.'
