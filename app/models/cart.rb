35e5f9626caa273b240e9da524b5ab83fc5cb6cc
Dimm <tds050680@gmail.com>
2018-02-14 07:48:10 +0200

    Add decrement to cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,6 +11,16 @@ class Cart < ApplicationRecord
     current_item
   end
 
+  def remove_product(line_item_id)
+    current_item = line_items.find(line_item_id)
+    if current_item.quantity > 1
+      current_item.quantity -= 1
+    else
+      current_item.destroy
+    end
+    current_item
+  end
+
   def total_price
     line_items.to_a.sum { |item| item.total_price }
   end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -7,4 +7,12 @@ class LineItem < ApplicationRecord
     product.price * quantity
   end
 
+  def decrement
+    if self.quantity > 1
+      self.quantity -= 1
+    else
+      self.destroy
+    end
+      self
+  end
 end
